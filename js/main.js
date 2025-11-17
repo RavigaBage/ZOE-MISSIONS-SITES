35e310e2786e908df7def053a79e625a689d430a
sherlocK <theophilus.tetteh377@gmail.com>
2025-11-17 15:30:15 +0000

    language models added

--- js/main.js
+++ js/main.js
@@ -269,4 +269,44 @@ counters.forEach(counter => {
             pauseOnHover: false,   // continuous even on hover
             fade: false,           // no fade
         });
+        document.querySelector('#select_language').addEventListener('change',(e)=> loadLanguage(e.target.value));
+        async function loadLanguage(lang = "en") {
+    try {
+        const res = await fetch('js/language.json',
+            {
+                method: 'GET',
+                cache: 'no-cache'
+            }
+        );
+        if (!res.ok) return console.error("Language file not found");
+        
+        const data = await res.json();
+        const langObj = data[lang]; // example: data["fr"], data["ar"], etc.
+
+        if (!langObj) return console.error("Language not found in JSON");
+
+        Object.entries(langObj).forEach(([key, value]) => {
+            // check if a dom element has a class that matches the key
+            const el = document.querySelector(`.${key}`);            
+                if (el) {
+                    el.classList.add('animate_x');
+                    el.classList.add('hide');   
+                    setTimeout(()=>{
+                        if (value.includes("<") && value.includes(">")) {
+                        el.innerHTML = value;
+                        } else {
+                            el.textContent = value;
+                        }
+                        el.classList.remove('hide'); 
+                    },900)
+                }
+           
+
+        });
+
+    } catch (err) {
+        console.error("Language loading error:", err);
+    }
+}
+
 
