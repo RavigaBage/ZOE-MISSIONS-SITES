5908cdfebccadad91588fa8a393a02130130eef9
sherlocK <theophilus.tetteh377@gmail.com>
2026-02-21 21:05:15 +0000

    profiles updated

--- code-an-email-newsletter-subscription-system-with-node-express-nodemailer-main/src/server.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-import ExpressService from './services/ExpressService.js';
-import { connectDB } from './services/dataBase.js';
-
-await connectDB(); 
-dotenv.config();
-new ExpressService().start();
